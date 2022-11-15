3684a1c73766be44d2d1a26012938751c8ef3e56
Matthew Guibord <guibord2@illinois.edu>
2022-11-15 19:05:54 +0000

    Node struct additions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 // Main
 
 #include <iostream>
+#include "src/Node.h"
 
 using namespace std;
 
 int main()
 {
     cout<<"Hello World" << endl;
-
+    Node node1 = Node("Matt");
+    node1.add_edge("Desi");
+    node1.add_edge("Ruben");
+    node1.print();
+    cout << node1.edge_exists("Ruben") << endl;
+    cout << node1.edge_exists("Desi") << endl;
+    cout << node1.edge_exists("Michael") << endl;
     return 0;
 }
--- src/Node.h
+++ src/Node.h
@@ -1,9 +1,45 @@
 #pragma once
-using namespace std
+using namespace std;
 
 #include <vector>
+#include <string>
+#include <iostream>
+
 struct Node{
     string title_;
     vector<string> edge_list_;
     bool explored_;
-}
+
+    //Constructor:
+    Node(string title) : title_(title) { }
+
+    //Adding an edge to a node
+    void add_edge(string page_name){
+        edge_list_.push_back(page_name);
+    }
+
+    //Returns bool if desired edge exists in edge list:
+    bool edge_exists(string page_name){
+        for (auto & element : edge_list_){
+            if(element == page_name){return true;}
+        }
+        return false;
+    }
+
+    //Prints the contents of the Node
+    void print(){
+        cout << "Title: " << title_ << endl;
+        cout << "Edges: ";
+        for (auto & elem : edge_list_)
+            cout << elem << " ";
+        cout << endl;
+        
+    }
+
+};
+
+
+
+
+
+
