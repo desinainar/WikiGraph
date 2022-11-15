b2d2ea5cf4a8b7dd75d85d2a38519ea1d299b399
Zavaonah <rubenzneyroud@gmail.com>
2022-11-15 18:17:16 +0000

    Read-in from tsv

---
+++ src/Graph.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<Node.h>
+#include <vector>
+#include <iostream>
+class Graph {
+    public:
+    void readIn();
+    private:
+    std::vector<Node> node_list;
+};
+//reaed in nodes, then bfs through each nodes adjacency list until we find target article, then 
