6e295c408c4e5ab011a532c2c09b257c58aa1f33
Zavaonah <rubenzneyroud@gmail.com>
2022-11-30 04:14:38 +0000

    Cleanup and preparing for branch

--- src/Graph.h
+++ src/Graph.h
@@ -7,17 +7,15 @@ class Graph {
     public:    
     
     //Check to see if two graphs are identical
-    bool operator==(Graph g1);
+    //bool operator==(Graph g1);
 
     //read a tsv and load into
     void readIn(std::string tsv);
     void print();
-    std::vector<Node> Djikstras(std::string source, std::string target);
-    Node findNode(std::string title);
-    std::vector<Node> get_node_list();
+    std::vector<Node> Bfs(std::string source, std::string target);
+
 
     private:
-    std::vector<Node> node_list; //Node contains adjacency lsit
     std::map<std::string, Node> node_map;
 };
 //reaed in nodes, then bfs through each nodes adjacency list until we find target article, then 
