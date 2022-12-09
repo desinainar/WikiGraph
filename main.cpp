6b5ae70d1dda7cea84c49e495acd4b94d0d9d1ed
Matthew Guibord <guibord2@illinois.edu>
2022-12-09 21:25:52 +0000

    Main almost complete

--- main.cpp
+++ main.cpp
@@ -1,44 +1,46 @@
 #include "Graph.h"
 int main()
 {
-    // g++ -o main main.cpp src/Node.cpp src/Graph.cpp -Isrc -g    to compile without make file
+    // g++ -g -o main main.cpp src/Node.cpp src/Graph.cpp -Isrc    to compile without make file
     // ./main                                                to run
+   
+   
     Graph g = Graph();
-    g.readIn("t");
-    //g.print();
-    //Graph badreadin = Graph();
-    //badreadin.readIn("gabran.txt");
-    //badreadin.print();
-    /*
-    std::cout<<"---------------------------------\n\n";
-    std::vector<Node> answer = g.Bfs("1", "5");
-    for (Node node : answer) {
-        std::cout << node.title_ << " -> ";
-    }
-    std::cout<<"\n---------------------------------\n";
-    std::map<string, double> central = g.Brandes();
-    if (answer.empty()) {std::cout << "empty\n";}
-    else {
-        for (Node node : answer) {
-            std::cout << node.title_ << " -> "; 
-        }
-        std::cout << "\n";
-        for (auto it : central) {
-            std::cout << it.first << ": " << it.second << " ->";
-        }
-    }*/
-    std::cout<<"\n---------------------------------\n";
-    std::vector<std::set<Node>> connected = g.Tarjans();
-    int count = 1;
-    for (auto component : connected) {
-        if (component.empty()) {std::cout << "Empty\n";}
-        std::cout << "Component " << count << ":\n";
-        count++;
-        for (Node node : component) {
-            std::cout << node.title_ << " - ";
-        }
-        std::cout << "\n";
+    g.readIn("links.txt");
+    int starting_page;
+    int ending_page;
+    
+    cout << "Enter number between 0 and 4586 for starting page: ";
+    cin >> starting_page;
+    while(starting_page < 0 || starting_page > 4586){
+        cout << "Sorry, invalid starting number, please try again: ";
+        cin >> starting_page;
     }
     
+    cout << "\nEnter number between 0 and 4586 for ending page: ";
+    cin >> ending_page;
+    while(ending_page < 0 || ending_page > 4586){
+        cout << "Sorry, invalid ending number, please try again: ";
+        cin >> ending_page;
+    }
+    
+    
+    
+    std::map<std::string, Node> graph_map = g.get_map();
+    auto it_start = graph_map.begin();
+    auto it_end = graph_map.begin();
+    for (int i = 0; i < starting_page; i++){it_start++;}
+    for(int j = 0; j < ending_page; j++){it_end++;}
+    std::string decoded_start = (it_start->second).utf8_decode(it_start->first);
+    std::string decoded_end = (it_end->second).utf8_decode(it_end->first);
+    cout << "\nStarting Page Chosen: " << decoded_start << endl;
+    cout << "Ending Page Chosen: " << decoded_end << endl;
+
+    //BFS Example:
+    std::vector<Node> bfs_result = g.Bfs(it_start->first, it_end->first);
+    for(auto & elem : bfs_result){
+        cout << elem.title_ << endl;
+    }
+
     return 0;
 }
